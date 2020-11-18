5c59b9d4bb5cf51f3728986ce5f219024d7be62e
Alessandro <internicola.alessandro@gmail.com>
2020-11-18 16:30:44 +0100

    Esercizio Completato
    Creato il ciclo for per la visualizzazione a schermo della lista ordinata. Usato il ciclo while per determinare la posizione del nome inserito. Ripulito il codice ed usata una ol al posto di ul per pulizia

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,3 @@
-/// Esercizio ordinamento nomi
-
-
-
-//Chiedi all'utente il cognome
-//inseriscilo in un array con altri cognomi
-//stampa la lista ordinata alfabeticamente
-//scrivi anche la posizione della lista in cui il nuovo utente si trova
-
-
-
-
-// Chiedere all’utente il cognome
-// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
-// stampa la lista ordinata alfabeticamente
-// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
-
-
-
 var userNameInput = prompt("Per favore, inserisci il tuo nome");
 
 ///CAPITALIZE PER UN SORT PIU PRECISO
@@ -24,42 +5,29 @@ var firstSurname = userNameInput.slice(0,1).toUpperCase();
 var restSurname = userNameInput.slice(1,userNameInput.length).toLowerCase();
 userNameInput = firstSurname + restSurname;
 
-
+/// PUSH + ORDINE ALFABETICO
 var nameList = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
-
-console.log(nameList);
-
 nameList.push(userNameInput);
-
-console.log(nameList);
-
 nameList.sort();
+// console.log(nameList); --- SORT SULL'ARRAY
 
-console.log(nameList);
 
+/// INDIVIDUAZIONE DELLA POSIZIONE DEL NOME NELL'ARRAY
 var i=0;
 while (userNameInput != nameList[i]) {
   i++;
-  console.log(i);
+}
+// console.log(nameList[i]); --- CONTROLLO DLE NOME CORRETTO
+
+///VISUALIZZARE LA LISTA COMPLETA
+for (var j = 0; j < nameList.length; j++) {
+  var itemName = nameList[j];
+  var userName = document.getElementById('list').innerHTML;
+  document.getElementById('list').innerHTML = userName + "<li>" + itemName + "</li>";
 }
 
-console.log(nameList[i]);
-
-var userName = document.getElementById('nomi');
-userName.innerHTML = nameList[i];
-
-
-
-
-
-
-
-
-
-
-
-
-
+/// +1 PERCHE L'ARRAY INIZIA IL CONTEGGIO DA 0
+document.getElementById('name').innerHTML = i+1;
 
 
 
