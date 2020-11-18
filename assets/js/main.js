9ff14551de8a75047873edfdf6dece663899d54f
Alessandro <internicola.alessandro@gmail.com>
2020-11-18 15:53:42 +0100

    Array.push + Capitalize + Sort + While
    Create le righe per la funzione di capitalize, input + push nell'array, usato la funzone sort per ordinare.
    Usato un ciclo while come contatore, visualizzato a schermo il nome inserito

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,51 @@
 
 
 
+//Chiedi all'utente il cognome
+//inseriscilo in un array con altri cognomi
+//stampa la lista ordinata alfabeticamente
+//scrivi anche la posizione della lista in cui il nuovo utente si trova
 
 
 
 
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 
 
 
+var userNameInput = prompt("Per favore, inserisci il tuo nome");
+
+///CAPITALIZE PER UN SORT PIU PRECISO
+var firstSurname = userNameInput.slice(0,1).toUpperCase();
+var restSurname = userNameInput.slice(1,userNameInput.length).toLowerCase();
+userNameInput = firstSurname + restSurname;
+
+
+var nameList = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+console.log(nameList);
+
+nameList.push(userNameInput);
+
+console.log(nameList);
+
+nameList.sort();
+
+console.log(nameList);
+
+var i=0;
+while (userNameInput != nameList[i]) {
+  i++;
+  console.log(i);
+}
+
+console.log(nameList[i]);
+
+var userName = document.getElementById('nomi');
+userName.innerHTML = nameList[i];
 
 
 
